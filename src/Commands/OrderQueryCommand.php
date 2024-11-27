718c5550a5ba5aaf27f526a36b765c04611d7bf6
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-11-27 22:51:52 +0100

    wip

---
+++ src/Commands/OrderQueryCommand.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Nidavellir\Mjolnir\Commands;
+
+use Illuminate\Console\Command;
+
+class OrderQueryCommand extends Command
+{
+    protected $signature = 'excalibur:order-query';
+
+    protected $description = '[Debug] - Queries an order';
+
+    public function handle()
+    {
+    }
+}
