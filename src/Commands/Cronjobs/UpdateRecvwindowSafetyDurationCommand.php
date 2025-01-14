fe14176676a49ebd8d353e6ae646733dfa6502bd
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-01-14 23:50:19 +0100

    Update UpdateRecvwindowSafetyDurationCommand.php

--- src/Commands/Cronjobs/UpdateRecvwindowSafetyDurationCommand.php
+++ src/Commands/Cronjobs/UpdateRecvwindowSafetyDurationCommand.php
@@ -17,12 +17,6 @@ class UpdateRecvwindowSafetyDurationCommand extends Command
         // Fetch the Binance admin account
         $account = Account::admin('binance');
 
-        if (! $account) {
-            $this->error('Binance admin account not found.');
-
-            return 1;
-        }
-
         // Get the server time from Binance API via the admin account
         $response = $account->withApi()->serverTime();
         $serverTime = json_decode($response->getBody(), true)['serverTime']; // Server time in milliseconds
@@ -35,6 +29,9 @@ class UpdateRecvwindowSafetyDurationCommand extends Command
 
         // Add 50% safety margin to the time difference
         $recvWindowMargin = $timeDifferenceMs + ($timeDifferenceMs * 0.50); // 50% safety margin
+
+        // Ensure the recvWindowMargin is at least 2000
+        $recvWindowMargin = max($recvWindowMargin, 2000);
 
         // Update the `recvwindow_margin` in the `api_systems` table
         ApiSystem::where('canonical', 'binance')->update([
