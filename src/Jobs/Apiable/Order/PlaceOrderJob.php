c9163430291e65658778c63786a57180d4fcb362
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-01-21 00:29:19 +0100

    Update PlaceOrderJob.php

--- src/Jobs/Apiable/Order/PlaceOrderJob.php
+++ src/Jobs/Apiable/Order/PlaceOrderJob.php
@@ -50,10 +50,10 @@ class PlaceOrderJob extends BaseApiableJob
 
         // Are we placing another MARKET, PROFIT, or MARKET-CANCEL order by mistake?
         if (in_array($this->order->type, ['MARKET', 'PROFIT', 'MARKET-CANCEL']) &&
-            $orders->whereIn('type', ['MARKET', 'PROFIT', 'MARKET-CANCEL'])
-                ->whereNotNull('exchange_order_id')
-                ->isNotEmpty()) {
-            throw new \Exception('A 2nd market, profit, or market-cancel order is trying to be created for the same position!');
+            $orders->where('type', $this->order->type)
+                   ->whereNotNull('exchange_order_id')
+                   ->isNotEmpty()) {
+            throw new \Exception('A 2nd order of the same type (' . $this->order->type . ') is trying to be created for the same position!');
         }
 
         // Are we trying to create another limit order more than the totalLimitOrders?
