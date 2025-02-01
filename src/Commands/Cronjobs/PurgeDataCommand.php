266f86006d8415e667730465f4967b7692ed22f0
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-02-01 10:52:36 +0100

    Update PurgeDataCommand.php

--- src/Commands/Cronjobs/PurgeDataCommand.php
+++ src/Commands/Cronjobs/PurgeDataCommand.php
@@ -30,34 +30,41 @@ class PurgeDataCommand extends Command
     public function handle()
     {
         // Calculate the purge date (2 days ago from now)
-        $purgeDate = now()->subDays(2)->format('Y-m-d H:i:s');
+        $purgeDate2DaysAgo = now()->subDays(2)->format('Y-m-d H:i:s');
+        $purgeDate10DaysAgo = now()->subDays(10)->format('Y-m-d H:i:s');
+        $purgeDate1MonthAgo = now()->subMonth()->format('Y-m-d H:i:s');
 
         $binaryLogStatus = '';
         $deletedJobQueueEntries = 0;
         $deletedApiRequestLogs = 0;
+        $deletedPriceHistoryLogs = 0;
 
         try {
             // Purge MySQL binary logs
-            DB::unprepared("PURGE BINARY LOGS BEFORE '{$purgeDate}';");
-            $binaryLogStatus = "Successfully purged MySQL binary logs older than 2 days (before {$purgeDate}).";
+            DB::unprepared("PURGE BINARY LOGS BEFORE '{$purgeDate2DaysAgo}';");
 
             // Purge data from core_job_queue table
             $deletedJobQueueEntries = DB::table('core_job_queue')
-                ->where('created_at', '<', $purgeDate)
+                ->where('created_at', '<', $purgeDate2DaysAgo)
                 ->delete();
 
             // Purge data from api_requests_log table
             $deletedApiRequestLogs = DB::table('api_requests_log')
-                ->where('created_at', '<', $purgeDate)
+                ->where('created_at', '<', $purgeDate2DaysAgo)
                 ->delete();
 
-            // Purge data from api_requests_log table
+            // Purge data from order_history table
             $deletedOrderHistoryLogs = DB::table('order_history')
-                ->where('created_at', '<', $purgeDate)
+                ->where('created_at', '<', $purgeDate10DaysAgo)
+                ->delete();
+
+            // Purge data from the price_history table
+            $deletedPriceHistoryLogs = DB::table('order_history')
+                ->where('created_at', '<', $purgeDate1MonthAgo)
                 ->delete();
 
             // Send Pushover notification with report
-            $this->sendReport($purgeDate, $deletedJobQueueEntries, $deletedApiRequestLogs, $deletedOrderHistoryLogs);
+            $this->sendReport($purgeDate2DaysAgo, $purgeDate10DaysAgo, $purgeDate1MonthAgo, $deletedJobQueueEntries, $deletedApiRequestLogs, $deletedOrderHistoryLogs, $deletedPriceHistoryLogs);
         } catch (\Exception $e) {
             $this->error('An error occurred: '.$e->getMessage());
 
@@ -75,10 +82,10 @@ class PurgeDataCommand extends Command
      * @param  int  $deletedApiRequestLogs
      * @return void
      */
-    protected function sendReport($purgeDate, $deletedJobQueueEntries, $deletedApiRequestLogs, $deletedOrderHistoryLogs)
+    protected function sendReport($purgeDate2DaysAgo, $purgeDate10DaysAgo, $purgeDate1MonthAgo, $deletedJobQueueEntries, $deletedApiRequestLogs, $deletedOrderHistoryLogs, $deletedPriceHistoryLogs)
     {
-        User::admin()->get()->each(function ($user) use ($deletedJobQueueEntries, $deletedApiRequestLogs, $deletedOrderHistoryLogs) {
-            $message = "Purge Summary - core_job_queue: {$deletedJobQueueEntries} entries, api_requests_log: {$deletedApiRequestLogs} entries, order_history: {$deletedOrderHistoryLogs}";
+        User::admin()->get()->each(function ($user) use ($deletedJobQueueEntries, $deletedApiRequestLogs, $deletedOrderHistoryLogs, $deletedPriceHistoryLogs) {
+            $message = "Purge Summary - core_job_queue: {$deletedJobQueueEntries} entries, api_requests_log: {$deletedApiRequestLogs} entries, order_history: {$deletedOrderHistoryLogs}, price_history: {$deletedPriceHistoryLogs}";
 
             $user->pushover(
                 message: $message,
