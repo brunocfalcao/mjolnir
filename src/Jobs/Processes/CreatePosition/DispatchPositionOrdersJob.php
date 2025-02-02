a6a4ca41094c8ec0e8138f1b062263eef2020485
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-02-03 00:16:53 +0100

    Update DispatchPositionOrdersJob.php

--- src/Jobs/Processes/CreatePosition/DispatchPositionOrdersJob.php
+++ src/Jobs/Processes/CreatePosition/DispatchPositionOrdersJob.php
@@ -54,7 +54,7 @@ class DispatchPositionOrdersJob extends BaseQueuableJob
          * This might happen in rare scenarios where the bot is closing and opening positions super
          * fast, like when the market is crashing or boosting.
          */
-        $openPositions = Position::active()->where('account_id', $account->id)->get()->count();
+        $openPositions = Position::active()->where('account_id', $this->account->id)->get()->count();
 
         if ($openPositions > $this->account->max_concurrent_trades) {
             throw new \Exception('Last open positions check failed: Total opened positions:' . $openPositions . '. Aborting orders dispatch!');
