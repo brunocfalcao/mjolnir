8c43ffc10fb8dcd3b3b56306c1b87dee1f8479df
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-02-06 18:23:50 +0100

    Update BaseQueuableJob.php

--- src/Abstracts/BaseQueuableJob.php
+++ src/Abstracts/BaseQueuableJob.php
@@ -2,13 +2,14 @@
 
 namespace Nidavellir\Mjolnir\Abstracts;
 
-use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Psr7\Response;
-use Nidavellir\Mjolnir\Exceptions\JustEndException;
-use Nidavellir\Mjolnir\Exceptions\JustResolveException;
-use Nidavellir\Thor\Models\CoreJobQueue;
 use Nidavellir\Thor\Models\User;
 use Psr\Http\Message\ResponseInterface;
+use Nidavellir\Thor\Models\CoreJobQueue;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\RequestException;
+use Nidavellir\Mjolnir\Exceptions\JustEndException;
+use Nidavellir\Mjolnir\Exceptions\JustResolveException;
 
 abstract class BaseQueuableJob extends BaseJob
 {
@@ -70,7 +71,13 @@ abstract class BaseQueuableJob extends BaseJob
             if ($e instanceof ConnectException) {
                 // For connection exceptions, we just need to retry again the job.
                 $this->coreJobQueue->updateToRetry(now()->addSeconds($this->workerServerBackoffSeconds));
+                return;
+            }
 
+            // If it's a cURL network error, we can also retry the job again.
+            if ($e instanceof RequestException && strpos($e->getMessage(), 'cURL error') != false) {
+                // For connection exceptions, we just need to retry again the job.
+                $this->coreJobQueue->updateToRetry(now()->addSeconds($this->workerServerBackoffSeconds));
                 return;
             }
 
