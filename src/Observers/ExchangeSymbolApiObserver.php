3d7ba002e65b67aedea2a52e26413b3373d5e659
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-02-13 21:05:20 +0100

    wip

--- src/Observers/ExchangeSymbolApiObserver.php
+++ src/Observers/ExchangeSymbolApiObserver.php
@@ -9,13 +9,12 @@ class ExchangeSymbolApiObserver
 {
     public function updated(ExchangeSymbol $exchangeSymbol)
     {
-        /**
-         * Update the price on the position that have this exchange symbol
-         */
         if ($exchangeSymbol->wasChanged('last_mark_price')) {
+            info('Updating positions at ' . now());
+
             Position::opened()
                 ->where('exchange_symbol_id', $exchangeSymbol->id)
-                ->update(['current_price' => $exchangeSymbol->last_mark_price]);
+                ->update(['last_mark_price' => $exchangeSymbol->last_mark_price]);
         }
     }
 }
--- src/Observers/PositionApiObserver.php
+++ src/Observers/PositionApiObserver.php
@@ -4,6 +4,7 @@ namespace Nidavellir\Mjolnir\Observers;
 
 use Illuminate\Support\Str;
 use Nidavellir\Thor\Models\Position;
+use Nidavellir\Thor\Models\User;
 
 class PositionApiObserver
 {
@@ -16,7 +17,25 @@ class PositionApiObserver
     public function updated(Position $position)
     {
         if ($position->wasChanged('last_mark_price')) {
-            // Compute magnet zones.
+            $magnetOrder = $position->orders()
+                ->where('type', 'LIMIT')
+                ->where('status', 'NEW')
+                ->when($position->direction == 'LONG', function ($query) use ($position) {
+                    return $query->where($position->last_mark_price >= 'magnet_activation_price');
+                })
+                ->when($position->direction == 'SHORT', function ($query) use ($position) {
+                    return $query->where($position->last_mark_price <= 'magnet_activation_price');
+                })->first();
+
+            if ($magnetOrder) {
+                User::admin()->get()->each(function ($user) use ($position, $magnetOrder) {
+                    $user->pushover(
+                        message: "Magnet activated for position {$position->parsedTradingPair}, ID: {$position->id} at price {$magnetOrder->magnet_activation_price}",
+                        title: "Magnet activated for position {$position->parsedTradingPair}",
+                        applicationKey: 'nidavellir_positions'
+                    );
+                });
+            }
         }
     }
 }
