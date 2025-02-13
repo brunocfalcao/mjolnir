150d5e2ba2876307c8b5abfa121ed5f20aca190d
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-02-13 21:07:09 +0100

    Update ExchangeSymbolApiObserver.php

--- src/Observers/ExchangeSymbolApiObserver.php
+++ src/Observers/ExchangeSymbolApiObserver.php
@@ -9,12 +9,8 @@ class ExchangeSymbolApiObserver
 {
     public function updated(ExchangeSymbol $exchangeSymbol)
     {
-        if ($exchangeSymbol->wasChanged('last_mark_price')) {
-            info('Updating positions at ' . now());
-
-            Position::opened()
-                ->where('exchange_symbol_id', $exchangeSymbol->id)
-                ->update(['last_mark_price' => $exchangeSymbol->last_mark_price]);
-        }
+        Position::opened()
+            ->where('exchange_symbol_id', $exchangeSymbol->id)
+            ->update(['last_mark_price' => $exchangeSymbol->last_mark_price]);
     }
 }
